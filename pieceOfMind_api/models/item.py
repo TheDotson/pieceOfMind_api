dbe04f575eb9f45f57ab0661969bf8333386151c
Michael Dotson <thedotson@icloud.com>
2021-03-11 20:09:19 -0600

    created seed data/fixtures

--- pieceOfMind_api/models/item.py
+++ pieceOfMind_api/models/item.py
@@ -5,7 +5,7 @@ from .room import Room
 from .collection import Collection
 
 class Item(models.Model):
-     collection_id = models.OneToOneField(Collection, on_delete=models.DO_NOTHING)
+     collection_id = models.ForeignKey(Collection, on_delete=models.DO_NOTHING)
      name = models.CharField(max_length=50)
      location = models.ForeignKey(Room, on_delete=models.DO_NOTHING, related_name='items')
      image = models.ImageField(upload_to='items', height_field=None, width_field=None, max_length=None, null=True)
